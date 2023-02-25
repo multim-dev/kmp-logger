bad55cb7bed9b472254a6a8d4e88c4e7722dffac
usbharu <64310155+usbharu@users.noreply.github.com>
2023-02-25 16:58:08 +0900

    fix: fix jvm cache

--- src/jvmMain/kotlin/dev/usbharu/logger/LoggerFactory.kt
+++ src/jvmMain/kotlin/dev/usbharu/logger/LoggerFactory.kt
@@ -1,7 +1,9 @@
 package dev.usbharu.logger
 
 actual object LoggerFactory {
+
+    private val cache = mutableMapOf<String, Logger>()
     actual fun getLogger(tag: String): Logger {
-        return Slf4JLogger(tag)
+        return cache.getOrPut(tag) { Slf4JLogger(tag) }
     }
 }
